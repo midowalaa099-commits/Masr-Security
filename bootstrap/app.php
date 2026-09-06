33ff1f727f3e66d70562d2794274901f97e459dd
Mohamed Walaa <midowalaa099@gmail.com>
2026-09-06 21:31:41 +0300

    Trust the immediate deployment proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: 'REMOTE_ADDR');
+
         $middleware->alias([
             'admin' => EnsureUserIsAdmin::class,
             'storefront' => ShareStorefrontData::class,
